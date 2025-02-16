30d092a5abed4ab90a739d06e85d1ebd39291bfd
AlexHak300gg <comp321089@mail.ru>
2025-02-16 19:02:24 +0400

    code: Profile complited
    Профель сделан

--- app/src/main/java/com/example/technostrelka/Class/ProfileActivity.java
+++ app/src/main/java/com/example/technostrelka/Class/ProfileActivity.java
@@ -1,26 +1,49 @@
 package com.example.technostrelka.Class;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.technostrelka.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private TextView fullNameTextView, loginTextView, emailTextView, mobilePhoneTextView;
+    private Button logoutButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize views
+        fullNameTextView = findViewById(R.id.fullNameTextView);
+        loginTextView = findViewById(R.id.loginTextView);
+        emailTextView = findViewById(R.id.emailTextView);
+        mobilePhoneTextView = findViewById(R.id.mobilePhoneTextView);
+        logoutButton = findViewById(R.id.logoutButton);
+
+        Intent intent = getIntent();
+        String fullName = intent.getStringExtra("FULL_NAME");
+        String login = intent.getStringExtra("LOGIN");
+        String email = intent.getStringExtra("EMAIL");
+        String mobilePhone = intent.getStringExtra("MOBILE_PHONE");
+
+        fullNameTextView.setText(fullName);
+        loginTextView.setText("Login: " + login);
+        emailTextView.setText("Email: " + email);
+        mobilePhoneTextView.setText("Mobile Phone: " + mobilePhone);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
         });
     }
 }
